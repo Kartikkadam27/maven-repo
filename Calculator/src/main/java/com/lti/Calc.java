8424b1cac53e4412039eecd4344badb3c7cc84d5
Kartikkadam27 <kkadam097@gmail.com>
2020-12-21 15:16:32 +0530

    added div method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -10,10 +10,15 @@ public class Calc {
 	public static int  mul(int a, int b) {
 		return a*b;
 	}
+	public static int  div(int a, int b) {
+		return a/b;
+	}
+	
 	public static void main(String args[]) {
 		System.out.println("Add "+add(10,20));
 		System.out.println("Sub "+sub(20,10));
 		System.out.println("mul "+mul(20,10));
+		System.out.println("div "+div(20,10));
 	}
 
 }
