9581b4069ca8df77ece78aa1f19732ccbd9601c7
Kartikkadam27 <kkadam097@gmail.com>
2020-12-21 15:09:24 +0530

    added mul meathod

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -7,9 +7,13 @@ public class Calc {
 	public static int  sub(int a, int b) {
 		return a-b;
 	}
+	public static int  mul(int a, int b) {
+		return a*b;
+	}
 	public static void main(String args[]) {
 		System.out.println("Add "+add(10,20));
 		System.out.println("Sub "+sub(20,10));
+		System.out.println("mul "+mul(20,10));
 	}
 
 }
